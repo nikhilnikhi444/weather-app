65ccb77623c85a0d18eaa0a5e92471f692cbbefe
nikhilnikhi444 <nikhilnikhi444@gmail.com>
2024-04-06 12:06:29 +0530

    weather app using js

--- weather.js
+++ weather.js
@@ -1,7 +1,9 @@
+
 const search = async ()=>{
     console.log('insdie search ');
     let cname = country.value
     console.log(cname);
+
     if(cname){
       const response =   await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cname}&appid=cf2113a8795f1208a4dca036c5c23bc2`)
       console.log(response);
@@ -91,20 +93,47 @@ const search = async ()=>{
       // wind speed
       let windsp = data.wind.speed
       console.log(windsp);
+      // timezone
+      let timezone = data.timezone
+      console.log(timezone);
+      // time stamp
+      let timestamp = data.dt
+      console.log(timestamp);
 
 
-      result.innerHTML=`<div class="card" style=" background-image: url(${background}); background-size: cover; background-repeat: no-repeat;">
+      function convertTimeStamp(timestamp, timezone) {
+         const date = new Date((timestamp + timezone) * 1000);
+     
+         const options = {
+             weekday: "long",
+             day: "numeric",
+             month: "long",
+             year: "numeric",
+             hour: "numeric",
+             minute: "numeric",
+             hour12: true,
+             timeZone: "UTC" 
+         };
+     
+         return date.toLocaleString("en-US", options);
+     }
+     
+
+     let formattedDate = convertTimeStamp(timestamp, timezone);
+
+      result.innerHTML=`<div class="card mt-2" style=" background-image: url(${background}); background-size: cover; background-repeat: no-repeat; max-height:80%;">
       <div class="icon">
         <img src="${icon}" class="weather-icon
         " alt="...">
         <h2>${wea}</h2>
         <h1>${degree}℃</h1>
-        <h6>min:${mindegree}℃  &  max:${maxdegree}℃</h6> 
-        <h5>feels like ${feeldegree}℃</h5>
+        <h6>min:${mindegree}°  &  max:${maxdegree}°</h6> 
+        <h5>feels like ${feeldegree}°</h5>
 
       </div>
      <div class="card-body w-100 p-0 ">
         <h1 class="card-title text-center">${name}</h1>
+        <h5 class="card-title text-center">${formattedDate}</h5>
         <div class="row">
           <div class="col-md-1"></div>
          <div class="col-md-5 text-center">
@@ -117,7 +146,7 @@ const search = async ()=>{
               <p><i class="fa-solid fa-gauge-high fa-xl"></i><span> ${press} hpa</span></p>
             </div>
          </div>
-         <div class="col-5 text-center">
+         <div class="col-md-5 text-center">
             <div class="mt-1">
               <h5> Humidity</h5>
               <p><i class="fa-solid fa-droplet fa-xl"></i><span> ${humid}%</span></p>
@@ -137,3 +166,56 @@ const search = async ()=>{
         alert('invaid input')
     }
 }
+
+const conames = ["bangalore", "delhi","kochi"];
+
+conames.forEach(coname => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${coname}&appid=cf2113a8795f1208a4dca036c5c23bc2`)
+        .then((respo) => {
+            respo.json().then((element) => {
+                console.log(element);
+
+                const icon = element.weather[0].icon;
+                const temperature = Math.floor(element.main.temp - 273.15);
+                const cityName = element.name;
+                const weath = element.weather[0].main;
+
+                fix1.innerHTML += `<div class="card w-100 mt-4 p-3" style="height:30%;backdrop-filter: blur(5px); background-color: rgba(255, 255, 255, 0.51);>
+                                        <div class="text-center text-light">
+                                            <img class="text-light" src="https://openweathermap.org/img/wn/${icon}@2x.png">
+                                            <h4 class="text-center">${weath}</h4>
+                                            <h3 class="text-center">${temperature}℃</h3>
+                                            <h4 class="text-center">${cityName}</h4>
+                                            
+                                        </div>
+                                    </div>`;                  
+            });
+        });
+});
+
+
+const counames = ["pune", "mumbai","chennai"];
+
+counames.forEach(couname => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${couname}&appid=cf2113a8795f1208a4dca036c5c23bc2`)
+        .then((respo) => {
+            respo.json().then((element) => {
+                console.log(element);
+
+                const icon = element.weather[0].icon;
+                const temperature = Math.floor(element.main.temp - 273.15);
+                const cityName = element.name;
+                const weath = element.weather[0].main;
+
+                fix2.innerHTML += `<div class="card w-100 mt-4 p-3" style="height:30%;backdrop-filter: blur(5px); background-color: rgba(255, 255, 255, 0.51);>
+                                        <div class="text-center text-light">
+                                            <img  src="https://openweathermap.org/img/wn/${icon}@2x.png">
+                                            <h4 class="text-center">${weath}</h4>
+                                            <h3 class="text-center">${temperature}℃</h3>
+                                            <h4 class="text-center">${cityName}</h4>
+                                    
+                                        </div>
+                                    </div>`;                  
+            });
+        });
+});
